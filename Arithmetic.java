ca3fca809b3d5e4239ff2146496ac24194778326
harshithareddynandyala-cpu <harshithareddynandyala@gmail.com>
2026-01-31 12:52:22 +0530

    Add files via upload
    frist commit

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Arithmetic{
+public static void main(String args[])
+{
+int a=5;
+int b=10;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a/b);
+System.out.println(a*b);
+}
+}
+
+
---
+++ Gun.java
@@ -0,0 +1,13 @@
+class Gun {
+public static void main(String[] args)
+{
+int a=5;
+int b=6;
+int sum = a + b;
+int difference = a-b;
+int multiplication = a*b;
+System.out.println("addition:"+sum);
+System.out.println("subtraction:"+difference);
+System.out.println("multiplication:"+multiplication);
+}
+}
